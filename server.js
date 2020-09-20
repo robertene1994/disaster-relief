5bf6b57ead10f79f5e11f4cd38a924c65b172d86
Robert <robertene1994@gmail.com>
2020-09-20 20:45:08 +0300

    Google Maps new fix -release-

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const fs = require('fs');
 const express = require('express');
 const http = require('http');
 const path = require('path');
-
-process.env.GOOGLE_MAPS_API_KEY = "axaxa";
-console.log(process.env.GOOGLE_MAPS_API_KEY);
-
-fs.readFile('dist/index.html', 'utf8', (_err, data) => {
-    const formatted = data.replace('GOOGLE_MAPS_API_KEY', process.env.GOOGLE_MAPS_API_KEY);
-    fs.writeFile('dist/index.html', formatted, 'utf8', (err) => {
-        if (err)
-            return console.log(err);
-    });
-});
+const fs = require('fs');
 
 const app = express();
 
@@ -25,6 +14,13 @@ app.get('*', (_req, res) => {
 const port = process.env.PORT || '4200';
 app.set('port', port);
 
+fs.readFile('dist/index.html', 'utf8', (_err, data) => {
+    const formatted = data.replace('GOOGLE_MAPS_API_KEY', process.env.GOOGLE_MAPS_API_KEY);
+    fs.writeFile('dist/index.html', formatted, 'utf8', (err) => {
+        if (err)
+            return console.log(err);
+    });
+});
 
 http.createServer(app).listen(port, () => {
     console.log(`Server started on port ${port}.`);
