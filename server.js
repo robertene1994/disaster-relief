27b65709502b8afaa7e666f775ab7d259f6dfa3f
Robert <robertene1994@gmail.com>
2020-09-20 20:33:56 +0300

    Google Maps API Configuration

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
+fs.readFile('dist/index.html', 'utf8', (err, data) => {
+    let searchString = 'GOOGLE_MAPS_API_KEY';
+    let re = new RegExp('^.*' + searchString + '.*$', 'gm');
+    let formatted = data.replace(re, process.env.GOOGLE_MAPS_API_KEY);
+
+    fs.writeFile(someFile, formatted, 'utf8', (err) => {
+        if (err)
+            return console.log(err);
+    });
+});
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -12,6 +23,7 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || '4200';
 app.set('port', port);
+
 
 http.createServer(app).listen(port, () => {
     console.log(`Server started on port ${port}.`);
